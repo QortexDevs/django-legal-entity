46b484438c6b1f4503bd3e29fc22d7d8946eb551
Nick Mitin <nick.mitin@gmail.com>
2021-01-17 14:47:00 +0000

    Initial commit

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.apps import apps
+
 from .views import GetLegalEntityInfoByInnView, GetBankInfoByBikView
 
-urlpatterns = [
-    path('2.0/get_legal_entity_info_by_inn',
+app_name = 'legal_entity'
+
+api_urlpatterns = [
+    path(app_name + '/get_legal_entity_info_by_inn',
          GetLegalEntityInfoByInnView.as_view()),
-    path('2.0/get_bank_info_by_bik',
+    path(app_name + '/get_bank_info_by_bik',
          GetBankInfoByBikView.as_view()),
 ]
